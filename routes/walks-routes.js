c68c041c0ecb124e1ae859335756289a9f3704cd
Devin Hinzo <devin.hinzo@gmail.com>
2021-01-12 13:35:09 -0500

    added two GET routes, basic error handling

--- routes/walks-routes.js
+++ routes/walks-routes.js
@@ -1,11 +1,69 @@
 const express = require('express')
 
-
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    console.log('GET request in walks')
-    res.json({message: 'it works!'})
+const DUMMY_DATA = [
+    {
+        id: 'w1',
+        title: 'Lakeview Cemetery',
+        description: 'Where all my friends are',
+        location: {
+            lat: 41.5138354,
+            lng: -81.5983687
+        },
+        address: '12316 Euclid Ave, Cleveland, OH 44106',
+        creator: 'u1'
+    },
+    {
+        id: 'w1',
+        title: 'Lakeview Cemetery',
+        description: 'Where all my friends are',
+        location: {
+            lat: 41.5138354,
+            lng: -81.5983687
+        },
+        address: '12316 Euclid Ave, Cleveland, OH 44106',
+        creator: 'u1'
+    },
+    {
+        id: 'w1',
+        title: 'Lakeview Cemetery',
+        description: 'Where all my friends are',
+        location: {
+            lat: 41.5138354,
+            lng: -81.5983687
+        },
+        address: '12316 Euclid Ave, Cleveland, OH 44106',
+        creator: 'u2'
+    }
+]
+
+router.get('/:wid', (req, res, next) => {
+    const walkId = req.params.wid
+    const walk = DUMMY_DATA.find(w => {
+        return w.id === walkId
+    })
+    if (!walk) {
+        const error = new Error('Could not find a walk for that id...')
+        error.code = 404
+        throw error
+    }
+    res.json({walk})
+})
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid
+    const userWalk = DUMMY_DATA.find(u => {
+        return u.creator === userId
+    })
+    if (!userWalk) {
+        if (!walk) {
+            const error = new Error('Could not find a walk for that user id...')
+            error.code = 404
+            return next(error)
+        }
+    }
+    res.json({userWalks})
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -5,7 +5,15 @@ const walkRoutes = require('./routes/walks-routes')
 
 const app = express()
 
-app.use(walkRoutes)
+app.use('/api/walks', walkRoutes)
+
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error)
+    }
+    res.status(error.code || 500)
+    res.json({message: error.message || 'An unknown error occurred...'})
+})
 
 app.listen(5000, () => {
     console.log('Listening for requests...')
