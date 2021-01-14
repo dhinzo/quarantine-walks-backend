b9674f54c81ed2ae6e5bfb0045de36948d5c19e9
Devin Hinzo <devin.hinzo@gmail.com>
2021-01-13 22:15:32 -0500

    added error handling for unsupported routes

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const walkRoutes = require('./routes/walks-routes')
+const HttpError = require('./models/http-error')
 
 const app = express()
 
 app.use(bodyParser.json())
 
 app.use('/api/walks', walkRoutes)
+
+app.use((req, res, next) => {
+    const error = new HttpError('could not find this route :(', 404)
+    throw error
+})
 
 app.use((error, req, res, next) => {
     if (res.headerSent) {
