a567bb09297c57c96b350508c0647d069fbd6f55
Devin Hinzo <devin.hinzo@gmail.com>
2021-01-13 23:51:32 -0500

    added basic login using dummy data

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const walkRoutes = require('./routes/walks-routes')
+const usersRoutes = require('./routes/users-routes')
+
 const HttpError = require('./models/http-error')
 
 const app = express()
@@ -9,6 +11,7 @@ const app = express()
 app.use(bodyParser.json())
 
 app.use('/api/walks', walkRoutes)
+app.use('/api/users', usersRoutes)
 
 app.use((req, res, next) => {
     const error = new HttpError('could not find this route :(', 404)
